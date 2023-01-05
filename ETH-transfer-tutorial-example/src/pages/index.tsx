b3b573301032718358f1a96c6debddeaaa261a8d
Shark <674224371@qq.com>
2023-01-05 21:22:56 +0800

    transfer ETH updated

--- ETH-transfer-tutorial-example/src/pages/index.tsx
+++ ETH-transfer-tutorial-example/src/pages/index.tsx
@@ -6,11 +6,15 @@ import React, {  useCallback, useState} from 'react';
 function App() {
 	//define global variables and relevant functions to set the variables by useState hook
   const [reddio, setReddio] = useState<Reddio | null>(null);
-	const [ethAddress,setEthAddress] = useState('')
-  const [privateKey, setPrivateKey] = useState('');
-  const [starkKey, setStarkKey] = useState('');
-  const [toStarkKey,setToStarkKey] = useState('0x30affb48fcf8bffaa40611a1f7a10e7ce9a4b0c98bae4dced219dd01d3db4fb');
-  const [tokenAmount, setTokenAmount] = useState(0);
+  const defaultArray = {
+    "ethAddressInput":"",
+    "privateKeyInput":"",
+    "starkKeyInput":"",
+    "toStarkKeyInput":"0x30affb48fcf8bffaa40611a1f7a10e7ce9a4b0c98bae4dced219dd01d3db4fb",
+    "tokenAmountInput":0,
+  }
+  const [eventArray,setEventArray] = useState(defaultArray);
+
 
   async function connectToWallet(){
     if (typeof window !== 'undefined') {
@@ -25,13 +29,8 @@ function App() {
 
        //Third, getting the account addresses from provider
        await provider.send('eth_requestAccounts', []);
-
-       //Fourth, we will set the ETH address to our form.
        const signer = provider.getSigner();
        const ethAddress = await signer.getAddress(); 
-       setEthAddress(ethAddress);
-
-       
 
        //Finally, we can create a new reddio instance (Goerli testnet) 
        //And assign global variable to it
@@ -43,9 +42,15 @@ function App() {
 
       const generateKey = async () => {
         try {
+          //Generate stark private key and public key 
+          //Store them into array
           const keypair = await reddio.keypair.generateFromEthSignature();
-          setStarkKey(keypair["publicKey"]);
-          setPrivateKey(keypair["privateKey"]);
+
+          //We will set ethAddress/starkKey/privateStarkKey on our array
+          setEventArray({...eventArray, 
+                        "ethAddressInput" : ethAddress,
+                         "starkKeyInput" : keypair["publicKey"],
+                         "privateKeyInput": keypair["privateKey"]})
         } catch (error) {
           console.error(error);
         }
@@ -61,9 +66,9 @@ function App() {
        //Deposit ETH to layer 2 if reddio object is defined
        await reddio?.apis.depositETH({
        //Your starkKey (public key on layer 2)
-       starkKey:starkKey,
+       starkKey:eventArray["starkKeyInput"],
        //Amounts you want to deposit
-       quantizedAmount:Number(tokenAmount),
+       quantizedAmount:Number(eventArray["tokenAmountInput"]),
        });
       }
 	 }
@@ -77,69 +82,65 @@ function App() {
 
       //transfer the amount to another starkKey
       const { data: res } = await reddio.apis.transfer({
-        starkKey: starkKey,
-        privateKey:privateKey,
-        amount: tokenAmount,
+        starkKey: eventArray["starkKeyInput"],
+        privateKey:eventArray["privateKeyInput"],
+        amount: eventArray["tokenAmountInput"],
         tokenId: assetId,
         type:'ETH',
-        receiver: toStarkKey,
+        receiver: eventArray["toStarkKeyInput"],
       });
       console.log(res);
 
     }
 
   }
-
-  async function withdrawETH(){
+  async function withdrawTokensFromL2(){
     if(reddio !== null){
-      //getting reddio's assetId and assetType for the ERC20 token
+      //Getting reddio's assetId and assetType for the ERC20 token
       const { assetId,assetType } = await reddio.utils.getAssetTypeAndId({
         type:"ETH",
       });
 
-      //Step 1: withdraw tokens from layer 2
+      //Withdraw tokens from layer 2
+      //It will usually take 4 hour to withdraw from layer 2 to withdraw area
       const { data: res } = await reddio.apis.withdrawalFromL2({
-        starkKey:starkKey,
-        privateKey:privateKey,
-        amount:tokenAmount,
+        starkKey:eventArray["starkKeyInput"],
+        privateKey:eventArray["privateKeyInput"],
+        amount:Number(eventArray["tokenAmountInput"]),
         tokenId:assetId,
         type:"ETH",
-        receiver:starkKey,
+        receiver:eventArray["starkKeyInput"],
       });
 
       console.log(res);
 
-      //Step 2: withdraw tokens from layer 1
-      const resTwo = await reddio.apis.withdrawalFromL1({
-        ethAddress: ethAddress,
+    }
+  }
+  async function withdrawTokensFromL1(){
+    if(reddio !== null){
+      //Getting reddio's assetId and assetType for the ETH token
+      const { assetId,assetType } = await reddio.utils.getAssetTypeAndId({
+        type:"ETH",
+      });
+
+      //Withdraw tokens from withdraw area on layer 1
+      const res = await reddio.apis.withdrawalFromL1({
+        ethAddress: eventArray["ethAddressInput"],
         assetType: assetType,
         type: 'ETH'
       })
+      console.log(res);
 
-      console.log(resTwo);
-
-      //This process usually takes about 4 hour
-      console.log("Withdraw successfully")
     }
   }
 	//when form values starts to change set the values to relevant variables
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = event.target;
-    if(name == 'starkKeyInput'){
-      setStarkKey(event.target.value);
-    }
-    else if(name == 'tokenAmountInput'){
-			setTokenAmount(Number(event.target.value));
-    }
-    else if(name == 'toStarkKey'){
-      setToStarkKey(event.target.value);
-    }
-    else if(name == 'privateKeyInput'){
-      setPrivateKey(event.target.value);
-    }
+    const eventName = event.target.name;
+    const eventValue = event.target.value;
+    setEventArray({...eventArray, [eventName] : eventValue})
+    
   };
 
-	//writing all the forms that we need
   return (
 <div className="App">
       <header className="App-header">
@@ -148,25 +149,26 @@ function App() {
 Connect wallet
 </button>
 <table>
+
   <tr>
     <td>ETH Address</td>
-    <td><input type="text" name="ethAddressInput" value={ethAddress} onChange={handleChange} disabled></input></td>
+    <td><input type="text" name="ethAddressInput" value={eventArray.ethAddressInput} onChange={handleChange} disabled></input></td>
   </tr>
   <tr>
     <td>Stark privateKey</td>
-    <td><input type="text" name="privateKeyInput" value={privateKey} onChange={handleChange}></input></td>
+    <td><input type="text" name="privateKeyInput" value={eventArray.privateKeyInput} onChange={handleChange}></input></td>
   </tr>
   <tr>
     <td>From starkKey</td>
-    <td><input type="text" name="starkKeyInput" value={starkKey} onChange={handleChange}></input></td>
+    <td><input type="text" name="starkKeyInput" value={eventArray.starkKeyInput} onChange={handleChange}></input></td>
   </tr>
   <tr>
     <td>To starkKey</td>
-    <td><input type="text" name="toStarkKeyInput" value={toStarkKey} onChange={handleChange}></input></td>
+    <td><input type="text" name="toStarkKeyInput" value={eventArray.toStarkKeyInput} onChange={handleChange}></input></td>
   </tr>
   <tr>
     <td>ETH Amount</td>
-    <td><input type="number" name="tokenAmountInput" value={tokenAmount} onChange={handleChange}></input></td>
+    <td><input type="number" name="tokenAmountInput" value={eventArray.tokenAmountInput} onChange={handleChange}></input></td>
   </tr>
 </table>
 <br/>
@@ -178,8 +180,13 @@ Connect wallet
     transfer L2 to L2
 </button>
 <br/>
-<button type="button" onClick={withdrawETH}>
-    withdraw L2 to L1
+<button type="button" onClick={withdrawTokensFromL2}>
+    withdraw from L2
+</button>
+
+<br/>
+<button type="button" onClick={withdrawTokensFromL1}>
+    withdraw from L1
 </button>
 
 
